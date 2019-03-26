a798d21e8d351d9146cbdf60d62ee2f85a65cfd4
unknown <mice.graff@gmail.com>
2019-03-26 11:30:01 +0000

    Creates Frames in Folder, WIP Background Change

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import sys
 import os
+import ctypes
 import cv2
 print(cv2.__version__)
 
-#FINDS THE VIDEO YOU WANT TO PLAY
+# FINDS THE VIDEO YOU WANT TO PLAY
 selectedVideo= "video.mp4"
 vid= cv2.VideoCapture(selectedVideo)
 
-#CREATES FOLDER TO STORE FRAMES IN
+# CREATES FOLDER TO STORE FRAMES IN
 if not os.path.exists('frames'):
     os.makedirs('frames')
+
+# CURRENT FRAME THAT ITS ON
+frameNumber= 0
+while(True):
+    # GET THE FRAME
+    ret, frame= vid.read()
+    # END OF FRAME
+    if not ret:
+        break
+    # STORING THE CURRENT FRAME
+    name= './frames/'+ str(frameNumber) + '.jpg'
+    print('beep boop making frame number ')
+    print(frameNumber)
+    cv2.imwrite(name, frame)
+    # NEXT FRAME
+    frameNumber += 1
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, name, 0)
